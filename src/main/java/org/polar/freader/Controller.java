0915136c33ae2fe4ba4becb528ef2df65661de32
Polaris <devpolaris1@gmail.com>
2024-09-19 22:46:46 +0200

    2024-09-19 | 22.46

--- src/main/java/org/polar/freader/Controller.java
+++ src/main/java/org/polar/freader/Controller.java
@@ -1,20 +1,17 @@
 package org.polar.freader;
 
 import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import java.nio.file.*;
-import javafx.scene.control.ComboBox;
 
 import java.io.File;
-
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
-
 import java.io.IOException;
 import java.util.Objects;
 
@@ -23,11 +20,14 @@ public class Controller {
     private Scene scene;
     private Parent root;
 
-    private ComboBox<String> difficultyComboBox;
-    public void comboValues() {
-        String[] difficulty = {"Easy", "Medium", "Hard"};
-        difficultyComboBox.setItems(FXCollections.observableArrayList(difficulty));
-        difficultyComboBox.setValue("Medium");
+    public void MenuToGame(javafx.event.ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("switch to game scene.");
+        //make popup menu to either choose old save or make new. boolean true or false then send to generatedatabase.
     }
 
     public void gameToMenu(javafx.event.ActionEvent event) throws IOException {
@@ -39,15 +39,27 @@ public class Controller {
         System.out.println("Exit to menu.");
     }
 
-    public void MenuToGame(javafx.event.ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-        System.out.println("switch to game scene.");
-        //make popup menu to either choose old save or make new. boolean true or false then send to generatedatabase.
+    public void exitProgram() {
+        System.exit(0);
     }
+
+    //Live in menu related above
+    //Live in game related below
+
+    @FXML
+    private Pane selectionBackground;
+    public void LoadNewGame(javafx.event.ActionEvent event) throws IOException {
+        selectionBackground.setVisible(false);
+    }
+
+    @FXML
+    private ComboBox<String> difficultyComboBox;
+    public void comboValues() {
+        String[] difficulty = {"Easy", "Medium", "Hard"};
+        difficultyComboBox.setItems(FXCollections.observableArrayList(difficulty));
+        difficultyComboBox.setValue("Medium");
+    }
+
     public void gameDifficulty(javafx.event.ActionEvent event) {
         String difficulty = difficultyComboBox.getValue();
         System.out.println("Difficulty: " + difficulty);
